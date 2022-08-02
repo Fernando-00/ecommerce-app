f5d1fe2b33f9b1fdfcc71e229bbcab20eba5656f
Fernando-00 <fernandorosell2024@u.northwestern.edu>
2022-08-02 00:05:54 -0500

    added popular items, sliding front page with images, newsletter, and footer

---
+++ src/data.js
@@ -0,0 +1,77 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://img.joomcdn.net/6dd2e312f3d4a48d4d764c9e0d8b477f7897d839_original.jpeg",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://www.dmarge.com/wp-content/uploads/2021/09/Dmarge-best-streetwear-brands-Adidas-920x920.jpg",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+        title: "SHIRT STYLE!",
+    },
+    {
+        id: 2,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+        title: "PANTS!",
+    },
+    {
+        id: 3,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+        title: "LIGHT JACKETS!",
+    },
+];
+
+
+export const popularProducts = [
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+    {
+        id: 2,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+    {
+        id: 3,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+    {
+        id: 4,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+    {
+        id: 5,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+    {
+        id: 6,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+    {
+        id: 7,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+    {
+        id: 8,
+        img: "https://cdn.shopify.com/s/files/1/1147/7882/files/London-Streetwear-2019_large.jpg?v=1553167766",
+    },
+];
